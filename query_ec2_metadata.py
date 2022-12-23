bfc3337ba4f0821297d1425bffdb7a17addc8433
Daniel Flook <daniel.flook@digital.hmrc.gov.uk>
2022-12-23 16:17:41 +0000

    BAU Add instance-tag command

--- query_ec2_metadata.py
+++ query_ec2_metadata.py
@@ -6,9 +6,65 @@ import sys
 from typing import Dict
 
 import requests
+from requests import HTTPError
 from requests.packages.urllib3 import Retry
 from requests.adapters import HTTPAdapter
 
+def instance_tags_ec2() -> Dict[str, str]:
+    import boto3
+
+    ec2 = boto3.client('ec2', region_name=ec2_metadata('placement/region'))
+
+    id_filter = {
+        'Name': 'resource-id',
+        'Values': [ec2_metadata('instance-id')]
+    }
+
+    paginator = ec2.get_paginator('describe_tags')
+
+    tags = {}
+    for page in paginator.paginate(Filters=[id_filter]):
+        tags |= {tag['Key']: tag['Value'] for tag in page['Tags']}
+    return tags
+
+def instance_tags() -> Dict[str, str]:
+    """
+    All tags on this instance
+
+    >>> instance_tags()
+    {
+        'Env': 'management',
+        'Name': 'bastion'
+    }
+    """
+
+    try:
+        return {
+            tag_name: instance_tag(tag_name)
+            for tag_name in ec2_metadata('tags/instance').splitlines()
+        }
+    except HTTPError as http_error:
+        if http_error.response.status_code == 404:
+            # Perhaps tags through metadata is not enabled
+            return instance_tags_ec2()
+        raise
+
+def instance_tag(tag: str) -> str:
+    """
+    Get the value of a specific tag
+
+    >>> instance_tag('Env')
+    'management'
+
+    """
+
+    try:
+        return ec2_metadata(f'tags/instance/{tag}')
+    except HTTPError as http_error:
+        if http_error.response.status_code == 404:
+            # Perhaps tags through metadata is not enabled
+            return instance_tags_ec2()[tag]
+        raise
 
 def instance_identity_document() -> Dict[str, str]:
     """
@@ -56,24 +112,22 @@ def imds(path):
     retry_strategy = Retry(total=14, status_forcelist=[429], backoff_factor=0.01)
     adapter = HTTPAdapter(max_retries=retry_strategy)
 
-    http = requests.Session()
-    http.mount("http://", adapter)
+    session = requests.Session()
+    session.mount("http://", adapter)
 
-    token_response = http.put(
+    token_response = session.put(
         f"http://169.254.169.254/latest/api/token",
         headers={"X-aws-ec2-metadata-token-ttl-seconds": "300"},
         timeout=10,
     )
-    if token_response.status_code != 200:
-        raise Exception(token_response.content.decode())
+    token_response.raise_for_status()
 
-    response = http.get(
+    response = session.get(
         f"http://169.254.169.254/latest{path}",
         headers={"X-aws-ec2-metadata-token": token_response.content.decode()},
         timeout=10,
     )
-    if response.status_code != 200:
-        raise Exception(response.content.decode())
+    response.raise_for_status()
 
     return response.content.decode()
 
@@ -87,6 +141,8 @@ def main():
 
     if command_name == "instance-identity":
         sys.stdout.write(instance_identity(sys.argv[1]))
+    elif command_name == "instance-tag":
+        sys.stdout.write(instance_tag(sys.argv[1]))
     else:
         sys.stdout.write(ec2_metadata(sys.argv[1]))
 
